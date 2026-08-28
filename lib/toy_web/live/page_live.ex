41ac257636084a71333abfa96a68b160d1fbc0a0
Piotrek Szyszko <piotrszyszko86@gmail.com>
2026-08-28 21:33:08 +0200

    Add LiveView name assign and reverse button
    Initialize a :imię assign in mount with "Piotrek", update render to greet using @imię, add a button (phx-click="odwraca") and implement handle_event("odwraca") to reverse the name string. Adds a simple interactive demo of LiveView assigns and events using Polish labels.

--- lib/toy_web/live/page_live.ex
+++ lib/toy_web/live/page_live.ex
@@ -1,9 +1,20 @@
 defmodule ToyWeb.PageLive do
   use ToyWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    socket = assign(socket, imię: "Piotrek")
+    {:ok, socket}
+  end
+
   def render(assigns) do
     ~H"""
-    <h1 class="text-3xl">Witaj Świecie!</h1>
+    <h1 class="text-3xl">Witaj {@imię}!</h1>
+    <.button phx-click="odwraca" class="btn btn-info">Odwróć</.button>
     """
+  end
+
+  def handle_event("odwraca", _params, socket) do
+    socket = assign(socket, imię: String.reverse(socket.assigns.imię))
+    {:noreply, socket}
   end
 end
