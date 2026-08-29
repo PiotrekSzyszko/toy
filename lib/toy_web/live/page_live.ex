b0a0035fa753bb03c58fca3f621e25aa47d7df28
Piotrek Szyszko <piotrszyszko86@gmail.com>
2026-08-29 15:45:32 +0200

    Use LiveView stream for food items
    Replace the manual :owoce assign with a LiveView stream. mount/3 now calls stream(socket, :pokarmy, ...) with maps containing id and nazwa. The template was updated to use phx-update="stream", iterating over @streams.pokarmy and using the generated dom_id for each <li>. Removes the previous Enum.filter-based list in favor of a stream-backed collection.

--- lib/toy_web/live/page_live.ex
+++ lib/toy_web/live/page_live.ex
@@ -2,18 +2,23 @@ defmodule ToyWeb.PageLive do
   use ToyWeb, :live_view
 
   def mount(_params, _session, socket) do
-    # logikę wykonywać w mount/3 ponieważ render/1 ma być maksymalnie głupi!
-    owoce =
-      Enum.filter(~w(jabłko banan marchewka cytryna), fn owoc -> owoc != "banan" end)
+    socket =
+      stream(socket, :pokarmy, [
+        %{id: 1, nazwa: "jabłko"},
+        %{id: 2, nazwa: "banan"},
+        %{id: 3, nazwa: "cytryna"},
+        %{id: 4, nazwa: "morela"},
+        %{id: 5, nazwa: "sliwka"},
+        %{id: 6, nazwa: "arbuz"}
+      ])
 
-    socket = assign(socket, owoce: owoce)
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <ul class="p-4 bg-yellow-300">
-      <li :for={owoc <- @owoce}>{owoc}</li>
+    <ul id="strumien-jedzenia" phx-update="stream">
+      <li :for={{dom_id, pokarm} <- @streams.pokarmy} id={dom_id}>{pokarm.nazwa}</li>
     </ul>
     """
   end
